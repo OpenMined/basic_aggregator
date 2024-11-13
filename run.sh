de97e4fc9bba39c5a210e6cba2e561d58429ddcd
Ionésio Junior <ionesiojr@gmail.com>
2024-11-13 11:39:33 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # this will create venv from python version defined in .python-version
 uv venv
 
-uv pip install syftbox
+uv pip install syftbox --quiet
 
 # run app using python from venv
 uv run main.py
