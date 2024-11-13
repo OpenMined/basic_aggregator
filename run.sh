6117879d58e58ed56d1d306175b3d921d716022d
Ionésio Junior <ionesiojr@gmail.com>
2024-11-13 19:15:44 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
+set -e
 
-# this will create venv from python version defined in .python-version
-uv venv
+if [ ! -d ".venv" ]; then
+    echo "Virtual environment not found. Creating one..."
+    uv venv -p 3.12 .venv
+    echo "Virtual environment created successfully."
+    uv pip install -U syftbox --quiet
+    # uv pip install -r requirements.txt
+else
+    echo "Virtual environment already exists."
+fi
 
-uv pip install syftbox --quiet
+. .venv/bin/activate
 
-# run app using python from venv
-uv run main.py
+# # run app using python from venv
+echo "Running basic_aggregator with $(python3 --version) at '$(which python3)'"
+uv run python3 main.py
+
+# # deactivate the virtual environment
+deactivate
