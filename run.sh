a47f4306fa7e1e7e2970ed98853abe8978a47239
Ionésio Junior <ionesiojr@gmail.com>
2024-11-14 17:16:46 -0300

    Update run.sh
    - Move pip install outside of if conditional
    - Replace uv run python3 main.py -> python3 main.py

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ if [ ! -d ".venv" ]; then
     echo "Virtual environment not found. Creating one..."
     uv venv -p 3.12 .venv
     echo "Virtual environment created successfully."
-    uv pip install -U syftbox --quiet
     # uv pip install -r requirements.txt
 else
     echo "Virtual environment already exists."
@@ -13,9 +12,11 @@ fi
 
 . .venv/bin/activate
 
+uv pip install -U syftbox --quiet
+
 # # run app using python from venv
 echo "Running basic_aggregator with $(python3 --version) at '$(which python3)'"
-uv run python3 main.py
+python3 main.py
 
 # # deactivate the virtual environment
 deactivate
